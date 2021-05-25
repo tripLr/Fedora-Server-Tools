b0247ee646205aa65c50fa3fb5bd5ce46a1e1320
tripLr <tripLr.dev@gmail.com>
2021-05-24 19:25:18 -0700

    Update f2b-unban.sh

--- f2b-unban.sh
+++ f2b-unban.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
 
-# f2b-unban copyright triplr
-# fail2ban unban user and jail jail optional
-#
+# f2b-unban ver 0.1  
+# copyright github.com/triplr
+
+echo " fail2ban unban user and jail , jail optional "
+
 # usage
 # f2b-unban user jail
 # f2b-unban $1   $2
-# 
+ 
 echo example
 echo f2b-unban triplr sshd
 
@@ -15,33 +17,47 @@ jail=$2
 
 echo "unbanning" $i "in jail" $jail
 
-
 function extract_ip () {
-				# command string to filter ip address out of output of user
-	last $i -i | 
-	awk '{print $3}' 
+                                # command string to filter ip address out of output of user
+        last $i -i | 
+        awk '{print $3}' 
 } 
 
 function filter_ip () {
-				# ip filter
-	grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" 
+                                # ip filter
+        grep -E -o "(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)" 
 
 }
 
 function sort_ip () {
-				# goal : match the sort with the output of the fail2ban jail ban list
-				# for now just pass to the fail2ban unban command
-	sort -u 
-}	
+                                # goal : match the sort with the output of the fail2ban jail ban list
+                                # for now just pass to the fail2ban unban command
+        sort -u 
+}
 
 function unban_ip () {
+while read -r line; 
+        do 
+                echo $line ;
+                fail2ban-client set sshd unbanip $line ; 
+        done
+}
 
-	fail2ban-client unban $1
-}	
+function help () {
+      
+	echo ' put help information here '
+	
+}
 
-extract_ip | filter_ip | sort_ip  
+#error control with exit codes
+# check if root
+# check if fail2ban-client exists
+# check if $1 is null
+# check if $1 user exists
 
-#for i in $(cat) ; do
-#    fal2ban-client unban $i
+if $1='--help'
+    then help
+    else extract_ip | filter_ip | sort_ip | unban_ip
+fi
 
-echo Fail2Ban unban IP 
+
